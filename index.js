6e717543d9c4408d412f5f1a9defee80e0f0d896
emilyjiji <92887308+emilyjiji@users.noreply.github.com>
2023-11-23 14:04:25 -0500

    backend routing basic setup

--- index.js
+++ index.js
@@ -4,16 +4,27 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
+
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
     console.log("Backend server is running!")
