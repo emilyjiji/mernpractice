d2b74b448aad952739a97c5b02938ce0f2ecd347
emilyjiji <92887308+emilyjiji@users.noreply.github.com>
2023-11-23 13:56:19 -0500

    mongoose and mongdb connection

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+app.listen(8800, () => {
+    console.log("Backend server is running!")
+})
